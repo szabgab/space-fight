5754d2a896bf8c99fde8ab86a14e31063ebbd6fc
Gabor Szabo <gabor@szabgab.com>
2012-11-07 19:20:59 +0200

    debug

--- python/space-fight.py
+++ python/space-fight.py
@@ -1,18 +1,48 @@
 #!/usr/bin/python
 
 import random
+debug = False
+
+def help():
+    print "One Dimensional Space Fight"
+    print "x - exit"
+    print "q - quit"
+    print "h - help"
+    print "s - show"
+    print "m - move"
+    print "n - next game"
+    print "d - debug"
 
 def play():
     hidden = random.randrange(1, 201, 1)
-    print "Secret: ", hidden
+    global debug
 
     while 1:
+        if debug:
+            print "Debug: ", hidden
+
         guess = raw_input("Guess: ")
         #print guess
 
-        if guess == 'x':
+        if guess == 'x' or guess == 'q':
             print "Thank you for playing"
             exit()
+
+        if guess == 'h':
+            help()
+            continue
+
+        if guess == 's':
+            print "Secret: ", hidden
+            continue
+
+        if guess == 'n':
+            print "Skipping this game "
+            return
+
+        if guess == 'd':
+            debug = not debug
+            continue
 
         try:
             guess = int(guess)
@@ -28,5 +58,9 @@ def play():
         except ValueError:
             print "Invalid input {:}".format(guess)
 
+def run():
+    help()
+    while 1:
+        play()
 
-play()
+run()
